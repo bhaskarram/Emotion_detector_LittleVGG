e6c080641246c5500bbd65d563570e5b41a2b59a
Bhaskar <bhaskar1208200r@gmail.com>
2021-04-07 01:27:40 +0530

    Emotion detection on webcam

--- face.py
+++ face.py
@@ -15,11 +15,13 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import RMSprop, SGD, Adam
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 from keras.models import load_model
-
+from time import sleep
+from keras.preprocessing.image import img_to_array
 import matplotlib.pyplot as plt
 import sklearn
 from sklearn.metrics import classification_report, confusion_matrix
 import numpy as np
+import cv2
 import os
 
 num_classes = 6
@@ -219,3 +221,51 @@ class_labels = validation_generator.class_indices
 class_labels = {v: k for k, v in class_labels.items()}
 classes = list(class_labels.values())
 print(class_labels)
+
+
+#On webcam
+
+face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+def face_detector(img):
+    # Convert image to grayscale
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+    if faces == ():
+        return (0,0,0,0), np.zeros((48,48), np.uint8), img
+    
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+
+    try:
+        roi_gray = cv2.resize(roi_gray, (48, 48), interpolation = cv2.INTER_AREA)
+    except:
+        return (x,w,y,h), np.zeros((48,48), np.uint8), img
+    return (x,w,y,h), roi_gray, img
+
+cap = cv2.VideoCapture(0)
+
+while True:
+
+    ret, frame = cap.read()
+    rect, face, image = face_detector(frame)
+    if np.sum([face]) != 0.0:
+        roi = face.astype("float") / 255.0
+        roi = img_to_array(roi)
+        roi = np.expand_dims(roi, axis=0)
+
+        # make a prediction on the ROI, then lookup the class
+        preds = classifier.predict(roi)[0]
+        label = class_labels[preds.argmax()]  
+        label_position = (rect[0] + int((rect[1]/2)), rect[2] + 25)
+        cv2.putText(image, label, label_position , cv2.FONT_HERSHEY_SIMPLEX,2, (0,255,0), 3)
+    else:
+        cv2.putText(image, "No Face Found", (20, 60) , cv2.FONT_HERSHEY_SIMPLEX,2, (0,255,0), 3)
+        
+    cv2.imshow('All', image)
+    if cv2.waitKey(1) == 13: #13 is the Enter Key
+        break
+        
+cap.release()
+cv2.destroyAllWindows()      
