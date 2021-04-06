3649e7340c3b1459ea76a115ac03f3b65d5003db
Bhaskar <bhaskar1208200r@gmail.com>
2021-04-06 23:52:52 +0530

    Data agumentation

---
+++ face.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  6 23:26:37 2021
+
+@author: Bhaskar
+"""
+
+from __future__ import print_function
+import keras
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
+import os
+
+num_classes = 6
+img_rows, img_cols = 48, 48
+batch_size = 16
+
+train_data_dir = './train'
+validation_data_dir = './validation'
+
+# Let's use some data augmentaiton 
+train_datagen = ImageDataGenerator(
+      rescale=1./255,
+      rotation_range=30,
+      shear_range=0.3,
+      zoom_range=0.3,
+      width_shift_range=0.4,
+      height_shift_range=0.4,
+      horizontal_flip=True,
+      fill_mode='nearest')
+ 
+validation_datagen = ImageDataGenerator(rescale=1./255)
+ 
+train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        color_mode = 'grayscale',
+        target_size=(img_rows, img_cols),
+        batch_size=batch_size,
+        class_mode='categorical',
+        shuffle=True)
+ 
+validation_generator = validation_datagen.flow_from_directory(
+        validation_data_dir,
+        color_mode = 'grayscale',
+        target_size=(img_rows, img_cols),
+        batch_size=batch_size,
+        class_mode='categorical',
+        shuffle=True)
